60518546ea30beaab9956816e051cea76446397e
jcson.pro <jcson.pro@gmail.com>
2023-02-11 21:12:16 +0900

    .

---
+++ front-end/add_two_numbers.js
@@ -0,0 +1,10 @@
+<script type="text/javascript">
+function doSomething() {
+    let a = document.getElementById('inputA').value;
+    let b = document.getElementById('inputB').value;
+    doucment.getElementById("valueA").innerHTML = a;
+    doucment.getElementById("valueB").innerHTML = b;
+    doucment.getElementById("valueC").innerHTML = Number(a) + Number(b);
+}
+
+</script>
